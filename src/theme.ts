c9e881c527aa7e0e63756f84f1bf479a23e36828
Mehdi Chtira <mehdi.chtira@gmail.com>
2021-02-25 13:11:30 +0100

    feat: config theme (wip)

---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+
+const theme = createMuiTheme({
+  palette: {
+    blue: '#002CC7',
+    green: '#0AAA93',
+    paper: '#E5E5E5',
+    red: '#FC4E40',
+    yellow: '#FED13F',
+  },
+})
+
+export default theme
