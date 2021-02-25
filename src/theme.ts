1d7451ff75f5104b3db068b372b721e222c2968e
Rémi Piubello <remipiubello@gmail.com>
2021-02-25 22:46:57 +0100

    feat(theme): add type for color palette

--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,22 @@
 import { createMuiTheme } from '@material-ui/core/styles'
 
+declare module '@material-ui/core/styles/createPalette' {
+  interface Palette {
+    blue: string
+    green: string
+    paper: string
+    red: string
+    yellow: string
+  }
+  interface PaletteOptions {
+    blue: string
+    green: string
+    paper: string
+    red: string
+    yellow: string
+  }
+}
+
 const theme = createMuiTheme({
   palette: {
     blue: '#002CC7',
