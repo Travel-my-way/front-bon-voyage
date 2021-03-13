af655afa793d2624e2246ca912f0e4522dcbba17
Mehdi Chtira <mehdi.chtira@gmail.com>
2021-03-13 21:17:55 +0100

    Feat/blue boxes (#3)
    * feat: remove unused files
    
    * feat: add missing color
    
    * feat: import and use new font
    
    * feat: add new stickers
    
    * feat: create homepage, and fill it with blueBoxes component
    
    * feat: add missing svg
    
    * feat: update css breakpoint
    
    * feat(blueboxes): blueboxes components is now responsive
    
    * fix(homepage): edit container media query breakpoint

---
+++ src/Components/index.ts
@@ -0,0 +1,1 @@
+export { default as BlueBoxes } from './BlueBoxes'
---
+++ src/Pages/Homepage.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
+
+import { BlueBoxes } from '../Components'
+import Flag from '../Assets/Logos/flag_bon_voyage.svg'
+
+const useStyles = createStyles(
+  makeStyles(({ palette, breakpoints }: Theme) => ({
+    container: {
+      [breakpoints.up('md')]: {
+        paddingLeft: '200px',
+        paddingRight: '200px',
+      },
+      [breakpoints.up('sm')]: {
+        paddingLeft: '120px',
+        paddingRight: '120px',
+      },
+      [breakpoints.only('xs')]: {
+        paddingLeft: '20px',
+        paddingRight: '20px',
+      },
+      backgroundColor: palette.paper,
+    },
+    flag: {
+      height: '100px',
+      marginBottom: '115px',
+      marginTop: '55px',
+      width: '315px',
+    },
+    title1: {
+      color: palette.blue,
+      fontSize: '50px',
+      fontStyle: 'normal',
+      fontWeight: 500,
+      marginBottom: '0',
+      marginTop: '0',
+      textAlign: 'center',
+      textShadow: `-4px 0px 0px ${palette.blue}`,
+    },
+    title2: {
+      boxSizing: 'border-box',
+      color: palette.paper,
+      fontSize: '50px',
+      fontStyle: 'normal',
+      fontWeight: 500,
+      lineHeight: '150%',
+      marginTop: '0',
+      textAlign: 'center',
+      textShadow: `-4px 0px 0px ${palette.yellow}`,
+      textStroke: `1.5px ${palette.blue}`,
+    },
+  }))
+)
+
+function Homepage(): JSX.Element {
+  const styles = useStyles()
+
+  return (
+    <div className={styles.container}>
+      <img src={Flag} className={styles.flag} />
+      <h1 className={styles.title1}>En route pour des voyages</h1>
+      <h2 className={styles.title2}>bas carbone</h2>
+      <BlueBoxes />
+    </div>
+  )
+}
+
+export default Homepage
