549854c9b4c0148205854e6381803ae59c9af21f
Mehdi Chtira <mehdi.chtira@gmail.com>
2021-03-24 15:41:42 +0100

    feat: create questions components and export it

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,2 +1,3 @@
-export { default as BlueBoxes } from './BlueBoxes'
-export { default as Footer } from './Footer'
+export { default as BlueBoxes } from './BlueBoxes';
+export { default as Footer } from './Footer';
+export { default as Questions } from './Questions/Questions';
