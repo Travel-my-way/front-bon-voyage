a49c3258c56e57af59660e7c5289c3ffc1bcd549
Mehdi Chtira <mehdi.chtira@gmail.com>
2021-04-17 00:03:10 +0200

    feat: remove textinput component and build the searchbar Component

--- src/Components/SearchBar/SearchBar.tsx
+++ src/Components/SearchBar/SearchBar.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Hidden, isWidthUp, withWidth } from '@material-ui/core';
+import { Hidden, isWidthUp, withWidth, Select, MenuItem, WithWidth } from '@material-ui/core';
 
 import YellowFlag from '../../Assets/Icons/yellowFlag.svg';
 import GreenFlag from '../../Assets/Icons/greenFlag.svg';
@@ -8,20 +8,37 @@ import RedWatch from '../../Assets/Icons/redWatch.svg';
 import MSN from '../../Assets/Icons/MSN.svg';
 import OuiAuTrain from '../../Assets/Logos/oui_au_train.svg';
 
-import { TextField } from './TextInput';
 import { CallToAction } from './CallToAction';
+import { AutoCompleteAddress } from './AutoCompleteAddress';
+import { DateTimePicker } from './DateTimePicker';
 
 const useStyles = makeStyles(({ breakpoints, palette }) => ({
   container: {
+    [breakpoints.up('md')]: {
+      marginLeft: 88,
+      marginRight: 88,
+    },
+    [breakpoints.up('sm')]: {
+      marginLeft: 60,
+      marginRight: 60,
+    },
+    [breakpoints.only('xs')]: {
+      marginLeft: 20,
+      marginRight: 20,
+    },
+    background: palette.paper,
     border: `solid 3px ${palette.blue}`,
     marginBottom: 232,
-    marginLeft: 88,
-    marginRight: 88,
     marginTop: 110,
     position: 'relative',
   },
   firstRow: {
-    height: 64,
+    height: 54,
+    marginTop: 12,
+    backgroundImage: `repeating-linear-gradient(to right, ${palette.blue} 0 3px, transparent 3px 12px)`,
+    backgroundPosition: 'bottom',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: '100% 1px',
   },
   logo: {
     height: 160,
@@ -29,27 +46,73 @@ const useStyles = makeStyles(({ breakpoints, palette }) => ({
     right: -100,
     top: -100,
     width: 160,
+    zIndex: 1,
+  },
+  margin: {
+    marginLeft: '30px',
+    marginRight: '30px',
   },
   secondRow: {
-    height: 55,
+    height: 48,
+    marginTop: 8,
+    backgroundImage: `repeating-linear-gradient(to right, ${palette.blue} 0 3px, transparent 3px 12px)`,
+    backgroundPosition: 'bottom',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'calc(100% - 100px) 1px',
+    backgroundPositionX: 'left',
+    [breakpoints.down('sm')]: {
+      backgroundSize: 'calc(100% - 70px) 1px',
+    },
+  },
+  textField: {
+    color: palette.blue,
+    fontFamily: 'Libre Franklin',
+    fontWeight: 400,
+    marginRight: 12,
+    '&:before': {
+      borderBottom: 'none',
+    },
+  },
+  textFieldContainer: {
+    alignItems: 'center',
+    display: 'flex',
+  },
+  textFieldLeftBottom: {
+    backgroundImage: `repeating-linear-gradient(to bottom, ${palette.blue} 0 3px, transparent 3px 12px)`,
+    backgroundPosition: '0 100%',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: '1px 100%',
+  },
+  textFieldLogo: {
+    marginRight: 12,
   },
   thirdRow: {
-    width: 200,
     [breakpoints.down('sm')]: {
       width: 80,
     },
     height: 51,
+    paddingLeft: 16,
+    width: 200,
   },
   thirdRowContainer: {
+    alignItems: 'center',
     display: 'flex',
     justifyContent: 'space-between',
+    marginLeft: 30,
   },
   travellerCTAContainer: {
     display: 'flex',
   },
 }));
 
-const SearchBar = ({ width }) => {
+type SelectedDestination = {
+  latlng: { lat: number; lng: number };
+  name: string;
+};
+
+type Props = WithWidth;
+
+const SearchBar = ({ width }: Props) => {
   const styles = useStyles();
 
   const placeholder = {
@@ -59,32 +122,77 @@ const SearchBar = ({ width }) => {
     travellers: isWidthUp('md', width) ? '2 voyageurs' : '2',
   };
 
+  const [numberOfVoyagers, setNumberOfVoyager] = useState(1);
+  const [arrivalLatlng, setArrivalLatlng] = useState<SelectedDestination | undefined>();
+  const [departureLatlng, setDepartureLatlng] = useState<SelectedDestination | undefined>();
+  const [selectedDate, handleDateChange] = useState(new Date());
+
+  const isButtonDisable = [numberOfVoyagers, arrivalLatlng, departureLatlng, selectedDate.valueOf()].some(
+    (value) => !value
+  );
+
+  const handleSubmit = () => {
+    console.log({
+      numberOfVoyagers,
+      arrivalLatlng,
+      departureLatlng,
+      selectedDate: selectedDate.valueOf(),
+    });
+  };
+
+  const getSelectItems = (numberOfItem = 10) => {
+    const renderLabel = (number: number) => {
+      if (number === 1) {
+        return isWidthUp('md', width) ? '1 voyageur' : 1;
+      }
+
+      return isWidthUp('md', width) ? `${number} voyageurs` : number;
+    };
+
+    const result = [
+      <MenuItem className={styles.textField} key={1} value={1} onClick={() => setNumberOfVoyager(1)}>
+        {renderLabel(1)}
+      </MenuItem>,
+    ];
+
+    for (let i = 2; i <= numberOfItem; i++) {
+      result.push(
+        <MenuItem className={styles.textField} onClick={() => setNumberOfVoyager(i)} value={i} key={i}>
+          {renderLabel(i)}
+        </MenuItem>
+      );
+    }
+
+    return result;
+  };
+
   return (
     <div className={styles.container}>
       <Hidden smDown>
         <img src={OuiAuTrain} className={styles.logo} />
       </Hidden>
-      <TextField
-        customClasses={styles.firstRow}
-        fullWidth
+      <AutoCompleteAddress
+        customClasses={`${styles.firstRow} ${styles.margin} ${styles.textField}`}
         logo={YellowFlag}
-        margin
         placeholder={placeholder.departure}
-        withBottomBorder
+        handleChanges={setDepartureLatlng}
       />
-      <TextField
-        customClasses={styles.secondRow}
-        fullWidth
+      <AutoCompleteAddress
+        customClasses={`${styles.secondRow} ${styles.margin} ${styles.textField}`}
         logo={GreenFlag}
-        margin
         placeholder={placeholder.arrival}
-        withBottomBorder
+        handleChanges={setArrivalLatlng}
       />
       <div className={styles.thirdRowContainer}>
-        <TextField logo={RedWatch} margin placeholder={placeholder.date} />
+        <DateTimePicker logo={RedWatch} selectedDate={selectedDate} handleChange={handleDateChange} />
         <div className={styles.travellerCTAContainer}>
-          <TextField logo={MSN} withLeftBorder placeholder={placeholder.travellers} customClasses={styles.thirdRow} />
-          <CallToAction />
+          <div className={`${styles.textFieldContainer} ${styles.textFieldLeftBottom} ${styles.thirdRow}`}>
+            <img className={styles.textFieldLogo} src={MSN} />
+            <Select value={numberOfVoyagers} className={styles.textField} defaultValue={1} fullWidth>
+              {getSelectItems(9)}
+            </Select>
+          </div>
+          <CallToAction isDisable={isButtonDisable} handleClick={handleSubmit} />
         </div>
       </div>
     </div>
