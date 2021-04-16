e4931e234aad69d4c8d8b9ff3ddc975cc4df2027
Mehdi Chtira <mehdi.chtira@gmail.com>
2021-04-17 00:02:21 +0200

    feat: add action on form submit

--- src/Components/SearchBar/CallToAction.tsx
+++ src/Components/SearchBar/CallToAction.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Button } from '@material-ui/core';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 import Arrow from '../../Assets/Icons/Arrow.svg';
 
 const useStyles = makeStyles(({ breakpoints, palette }) => ({
-  container: {
+  button: {
     [breakpoints.down('sm')]: {
       width: 80,
     },
     backgroundColor: palette.blue,
     width: 110,
+    position: 'relative',
+  },
+  disabled: {
+    backgroundColor: palette.grey['400'],
   },
   logo: {
     [breakpoints.down('sm')]: {
@@ -28,18 +33,29 @@ const useStyles = makeStyles(({ breakpoints, palette }) => ({
   },
 }));
 
-const CallToAction = (): JSX.Element => {
+type Props = {
+  handleClick: Function;
+  isDisable?: boolean;
+  isLoading?: boolean;
+};
+
+const CallToAction = ({ handleClick, isDisable, isLoading }: Props): JSX.Element => {
   const styles = useStyles();
 
   return (
     <Button
-      className={styles.container}
+      className={styles.button}
       fullWidth
+      disabled={!!isDisable}
+      onClick={() => {
+        handleClick();
+      }}
       classes={{
         root: styles.root,
+        disabled: styles.disabled,
       }}
     >
-      <img src={Arrow} className={styles.logo} />
+      {isLoading ? <CircularProgress size={20} /> : <img src={Arrow} className={styles.logo} />}
     </Button>
   );
 };
