fa871c1f75bf7d73f654d344909605d2f7aaafe0
Mehdi Chtira <mehdi.chtira@gmail.com>
2021-04-17 12:58:57 +0200

    feat: add wrapper arround homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
-
-import { Homepage } from './Pages'
+import React from 'react';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
+import { Homepage } from './Pages';
 
 function App(): JSX.Element {
-  return <Homepage />
+  return (
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <Homepage />
+    </MuiPickersUtilsProvider>
+  );
 }
 
-export default App
+export default App;
