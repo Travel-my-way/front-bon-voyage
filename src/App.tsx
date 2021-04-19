4a0a0150dc08bb0918de7d2d454f50b710c41009
Mehdi Chtira <mehdi.chtira@gmail.com>
2021-04-19 13:23:45 +0200

    fix: lint fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { MuiPickersUtilsProvider } from '@material-ui/pickers';
-import DateFnsUtils from '@date-io/date-fns';
 import { Homepage } from './Pages';
 
 function App(): JSX.Element {
