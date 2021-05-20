b91a9a3f40b65d9fdabe34b94fdd69baeda657ee
Mehdi Chtira <mehdi.chtira@gmail.com>
2021-05-20 14:24:56 +0200

    feat: update travel type

--- src/Types/Travel.d.ts
+++ src/Types/Travel.d.ts
@@ -2,10 +2,10 @@ type TravelCategory = 'Carpooling' | 'Car' | 'Train';
 
 interface TravelStep {
   arrival_date: number;
-  arrival_point: number[];
+  arrival_point: [number, number];
   arrival_stop_name: string;
   departure_date: number;
-  departure_point: number[];
+  departure_point: [number, number];
   departure_stop_name: string;
   distance_m: number | string;
   duration_s: number;
@@ -19,11 +19,11 @@ interface TravelStep {
 
 interface Travel {
   arrival_date: number;
-  arrival_point: number[];
+  arrival_point: [number, number];
   booking_link: string;
   category: TravelCategory[];
   departure_date: number;
-  departure_point: number[];
+  departure_point: [number, number];
   id: string | number;
   is_real_journey: boolean;
   journey_steps: TravelStep[];
