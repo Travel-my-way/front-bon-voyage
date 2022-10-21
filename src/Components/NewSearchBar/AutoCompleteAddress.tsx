7d2a4225a8d5053e4a23b23accdf38b7776d9df0
Mael <mael.thomas@data.gouv.fr>
2022-10-21 11:14:00 +0200

    Premier test de l'API komoot
    Suite : rendre un minimum correct l'affichage des suggestions

--- src/Components/NewSearchBar/AutoCompleteAddress.tsx
+++ src/Components/NewSearchBar/AutoCompleteAddress.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import AlgoliaPlaces from 'algolia-places-react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import classNames from 'classnames';
 
@@ -56,6 +55,40 @@ type Props = {
   customClasses?: string;
 };
 
+const PlaceInput = ({ placeholder, onChange }) => {
+  const [value, setValue] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+  useEffect(() => {
+    if (value.length < 3) return;
+    fetch(`https://photon.komoot.io/api/?q=${value}&limit=6&layer=city&lang=fr`)
+      .then((res) => res.json())
+      .then((json) => {
+        setSuggestions(json.features);
+      });
+  }, [value]);
+  return (
+    <div>
+      <input value={value} onChange={(e) => setValue(e.target.value)} placeholder={placeholder} />
+      <ul>
+        {suggestions.map((s) => (
+          <li
+            key={s.properties.osm_id}
+            style={{ cursor: 'pointer' }}
+            onClick={() =>
+              onChange({
+                value: s.properties.name,
+                latlng: { lat: s.geometry.coordinates[0], lng: s.geometry.coordinates[1] },
+              })
+            }
+          >
+            {s.properties.name} - {s.properties.country}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
 const AutoCompleteAddress = ({ customClasses = '', handleChanges, logo, placeholder }: Props): JSX.Element => {
   const styles = useStyles();
 
@@ -63,8 +96,8 @@ const AutoCompleteAddress = ({ customClasses = '', handleChanges, logo, placehol
     <div className={classNames(customClasses, styles.container)}>
       {logo && <img src={logo} className={styles.logo} />}
       <div className={styles.inputContainer}>
-        <AlgoliaPlaces
-          onChange={({ suggestion }: Suggestion) => {
+        <PlaceInput
+          onChange={(suggestion: Suggestion) => {
             handleChanges({
               name: suggestion.value,
               latlng: suggestion.latlng,
@@ -72,12 +105,6 @@ const AutoCompleteAddress = ({ customClasses = '', handleChanges, logo, placehol
           }}
           placeholder={placeholder}
           className={styles.input}
-          options={{
-            appId: config.appId,
-            apiKey: config.apiKey,
-            language: 'fr',
-            // Other options from https://community.algolia.com/places/documentation.html#options
-          }}
         />
       </div>
     </div>
